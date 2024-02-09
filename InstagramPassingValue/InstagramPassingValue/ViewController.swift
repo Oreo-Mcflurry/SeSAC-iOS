4866c8c95c85932fe507d066ff951b6ab61878de
유인호 <yuiop1029@naver.com>
2024-02-09 13:01:48 +0900

    init

---
+++ InstagramPassingValue/InstagramPassingValue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramPassingValue
+//
+//  Created by A_Mcflurry on 2/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
